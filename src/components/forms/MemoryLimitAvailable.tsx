03c3ca7fb2f095f1678a7eceeedd0458e1ce7486
David Edler <david.edler@canonical.com>
2025-02-02 14:06:36 +0100

    fix(instance) avoid memory leak on uploading external files. fixes #1087
    Signed-off-by: David Edler <david.edler@canonical.com>

---
+++ src/components/forms/MemoryLimitAvailable.tsx
@@ -0,0 +1,54 @@
+import { FC } from "react";
+import { useNotify } from "@canonical/react-components";
+import { useQuery } from "@tanstack/react-query";
+import { fetchResources } from "api/server";
+import { queryKeys } from "util/queryKeys";
+import { humanFileSize } from "util/helpers";
+import Loader from "components/Loader";
+import { limitToBytes } from "util/limits";
+import { useProject } from "context/project";
+
+const MemoryLimitAvailable: FC = () => {
+  const notify = useNotify();
+  const { project } = useProject();
+
+  const {
+    data: resources,
+    error,
+    isLoading,
+  } = useQuery({
+    queryKey: [queryKeys.resources],
+    queryFn: fetchResources,
+  });
+
+  if (isLoading) {
+    return <Loader text="Loading resources..." />;
+  }
+
+  if (error) {
+    notify.failure("Loading resources failed", error);
+  }
+
+  const getAvailableMemory = () => {
+    if (!project?.config["limits.memory"]) {
+      return resources?.memory.total;
+    }
+    if (!resources?.memory.total) {
+      return limitToBytes(project.config["limits.memory"]);
+    }
+    return Math.min(
+      resources.memory.total,
+      Number(limitToBytes(project.config["limits.memory"])),
+    );
+  };
+
+  const maxMemory = getAvailableMemory();
+
+  return maxMemory ? (
+    <>
+      Total memory: <b>{humanFileSize(maxMemory)}</b>
+    </>
+  ) : null;
+};
+
+export default MemoryLimitAvailable;
